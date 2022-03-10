a5299a4d280e9ee487353d99b0266fcb17f10d68
Dashynja0203 <9dashynja8@gmail.com>
2022-03-10 17:17:47 +0300

    backend

---
+++ backend/foodgram/users/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    email = models.EmailField('email адрес', unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    class Meta:
+        ordering = ('username',)
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
